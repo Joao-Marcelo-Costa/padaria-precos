609d559c37aa9a03dda68dec6b475a9cdce46cb3
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-06-04 16:20:42 -0300

    adiciona formulario

--- pages/materia-prima.js
+++ pages/materia-prima.js
@@ -1,1 +1,26 @@
 import "../src/style.css";
+const addButton = document.querySelector("#add_button");
+const formularioAdicionarInsumo = document.querySelector(
+  "#formularioAdicionarInsumo",
+);
+const inputNome = document.querySelector("#nome");
+const inputUnidade = document.querySelector("#unidade");
+const valorTotal = document.querySelector("#valorTotal");
+const quantidadePorEmbalagem = document.querySelector("#quantidade");
+const botaoCancelar = document.querySelector("#botaoCancelar");
+
+addButton.addEventListener("click", () => {
+  formularioAdicionarInsumo.classList.toggle("hidden");
+});
+
+botaoCancelar.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputNome.value = "";
+  inputNome.value = "";
+  inputNome.value = "";
+  formularioAdicionarInsumo.classList.toggle("hidden");
+});
+
+formularioAdicionarInsumo.addEventListener("submit", () => {
+  ("formulario enviado");
+});
