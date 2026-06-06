04f924265c4390bb38e7842669d7b826d0f2a10d
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-06-06 20:40:38 -0300

    creates add and delete button and adds functionability to delete button

--- pages/materia-prima.js
+++ pages/materia-prima.js
@@ -14,15 +14,37 @@ const corpoDaTabela = document.querySelector(".table_body");
 
 function adicionarInsumoNaTela(objetoInsumo) {
   const corpoDaTabela = document.querySelector(".table_body");
-  let linha = document.createElement("tr");
-  linha.innerHTML = `
+  let tr = document.createElement("tr");
+  tr.innerHTML = `
   <td>${objetoInsumo.nome}</td>
   <td>${objetoInsumo.unidade}</td>
   <td>R$${objetoInsumo.valorTotal},00</td>
   <td>${objetoInsumo.quantidadePorEmbalagem}</td>
-  <td>${objetoInsumo.valorTotal / objetoInsumo.quantidadePorEmbalagem}R$/${objetoInsumo.unidade}</td>
-  <td></td>`;
-  corpoDaTabela.appendChild(linha);
+  <td>${objetoInsumo.valorTotal / objetoInsumo.quantidadePorEmbalagem}R$/${objetoInsumo.unidade}</td>`;
+
+  const tdButtons = document.createElement("td");
+
+  const botaoEditar = document.createElement("button");
+  botaoEditar.innerHTML =
+    '<img src="/public/edit_icon.png" alt="botão de editar" />';
+  botaoEditar.addEventListener("click", () => {}); //vou criar o botão de editar depois de mexer no formulário de adição direito
+
+  const botaoDeletar = document.createElement("button");
+  botaoDeletar.innerHTML =
+    '<img src="/public/delete_icon.png" alt="botão de deletar" />';
+  botaoDeletar.addEventListener("click", () => {
+    let confirmação = confirm("deseja memso excluir esse item ?"); //depois criar algum tipo de aviso mais elaborado pra garantir que o usuàrio não delete sem querer
+    if (confirmação) {
+      tr.remove();
+      excluirProduto(objetoInsumo.id);
+    }
+  });
+
+  tdButtons.appendChild(botaoDeletar);
+  tdButtons.appendChild(botaoEditar);
+  tr.appendChild(tdButtons);
+
+  corpoDaTabela.appendChild(tr);
 }
 
 async function carregarInsumos() {
@@ -58,13 +80,17 @@ formularioAdicionarInsumo.addEventListener("submit", (e) => {
   let produtoParaCriar = {
     nome: inputNome.value,
     unidade: inputUnidade.value,
-    valorTotal: parseFloat(
-      inputValorTotal.value.replace(",", ".").replace(/[^\d.]/g, ""),
+    valorTotal: Number(
+      parseFloat(
+        inputValorTotal.value.replace(",", ".").replace(/[^\d.]/g, ""),
+      ).toFixed(2),
     ),
-    quantidadePorEmbalagem: parseFloat(
-      inputQuantidadePorEmbalagem.value
-        .replace(",", ".")
-        .replace(/[^\d.]/g, ""),
+    quantidadePorEmbalagem: Number(
+      parseFloat(
+        inputQuantidadePorEmbalagem.value
+          .replace(",", ".")
+          .replace(/[^\d.]/g, ""),
+      ).toFixed(2),
     ),
   };
   criarProduto(produtoParaCriar);
