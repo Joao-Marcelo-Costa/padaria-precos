602a882632e2a4892cd3754651cbf698cd5892f7
Joao-Marcelo-Costa <joaomacelocosta27@gmail.com>
2026-06-17 22:38:29 -0300

    cria tela de adicionar receita

--- src/script.js
+++ src/script.js
@@ -1,2 +1,9 @@
 import "./style.css";
 import "./main.css";
+
+const btAdicionarReceita = document.querySelector(".button_add_recepies");
+const janelaAdicionarReceita = document.querySelector(".add_recepie_window");
+
+btAdicionarReceita.addEventListener("click", () => {
+  janelaAdicionarReceita.showModal();
+});
